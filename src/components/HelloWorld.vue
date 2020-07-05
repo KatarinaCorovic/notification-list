e842b8b58e8f29b11e2ef0f7dcfd5263d2913262
Katarina <katarinatina369@gmail.com>
2020-07-05 21:26:57 +0200

    finished

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,13 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li :class="{'seen':notification.seen}" v-for="(notification, index) in notifications" v-bind:key="index">
+        <div class="">
+          <span v-on:click="markAsSeen(index)" class="circle"></span>
+          <span>{{notification.title}}</span>
+           <span class="date">{{notification.timestamp}}</span>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -35,24 +17,111 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      notifications:[
+        {
+          title:"Elon Musk sent you a message" ,
+          seen:true ,
+          timestamp: 1591106510
+        },
+        { 
+          title:"Mark Zuckerbeg sent you a message" ,
+          seen:false ,
+          timestamp: 1281106505
+        },
+        { 
+          title:"Milivoje would like to tag you in a photo" ,
+          seen:false ,
+          timestamp: 1361106303
+        },
+        { 
+          title:"Radivoje liked your comment" ,
+          seen:true ,
+          timestamp: 1331106300
+        },
+        { 
+          title:"BIA wants to know your location" ,
+          seen:true ,
+          timestamp: 1291106265
+        }
+      ],
+
+       dateSettings: {
+        hour:'numeric',
+        minute:'numeric'
+        }
+    }
+  },
+  methods: {
+    markAsSeen: function(index){
+      this.notifications[index].seen = true;
+    },
+    sortNotifications: function(){
+      this.notifications.sort(function(a,b){
+        return b.timestamp - a.timestamp; 
+      })
+    },
+    displayHumanReadDate: function(){ 
+      for(var i = 0; i < this.notifications.length; i++){
+        this.notifications[i].timestamp = new Date(this.notifications[i].timestamp * 1000).toLocaleString("en-US", this.dateSettings)
+      }
+    }
+  },
+  beforeMount(){    this.sortNotifications();
+    this.displayHumanReadDate();
   }
 }
+ 
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+ul{
+  list-style:none;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+li{
+  max-width: 550px;
+  text-align:left;
+  margin:0 auto;
+  background:rgb(21, 219, 209);
+  line-height: 50px;
+
 }
-li {
-  display: inline-block;
+
+li.seen{
+  background:rgb(156, 235, 231);
+}
+
+span{
+  vertical-align:middle;
+
+
+}
+
+.circle{
+  height:20px;
+  width:20px;
+  border-radius:50%;
+  background-color: rgb(0, 255, 0);
+  display:inline-block;
   margin: 0 10px;
+  cursor: pointer;
 }
-a {
-  color: #42b983;
+
+li.seen.circle{
+  background: rgb(118, 216, 118);
 }
+
+.date{
+  float:right;
+  margin-right: 15px;
+}
+
+
+
 </style>
